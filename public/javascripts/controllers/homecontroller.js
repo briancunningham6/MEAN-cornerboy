cf1334a01871a4e941f9619a8848722440e86027
Brian Cunningham <brianc@kseudo.com>
2013-09-07 16:22:43 +0100

    Added Angular.js to project

---
+++ public/javascripts/controllers/homecontroller.js
@@ -0,0 +1,11 @@
+/**
+ * Created with JetBrains PhpStorm.
+ * User: user
+ * Date: 07/09/2013
+ * Time: 16:13
+ * To change this template use File | Settings | File Templates.
+ */
+
+function HomeController($scope){
+
+}
